3461f94617390203626a2e7832a392a74e442529
devksingh <devksingh98@gmail.com>
2017-10-02 20:24:13 +0800

    Add files via upload

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -35,9 +35,9 @@ void KalmanFilter::Update(const VectorXd &z) {
     VectorXd z_pred = H_ * x_;
     VectorXd y = z - z_pred;
     MatrixXd Ht = H_.transpose();
-    MatrixXd S = H_ * P_ * Ht + R_;
-    MatrixXd Si = S.inverse();
     MatrixXd PHt = P_ * Ht;
+    MatrixXd S = H_ * PHt + R_;
+    MatrixXd Si = S.inverse();
     MatrixXd K = PHt * Si;
 
     //new estimate
@@ -73,10 +73,20 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   H_func << roh, phi, rohdot;
     
   VectorXd y = z - H_func;
+  //ensuring that y(1) is between -pi to pi (normalization)
+  while ((y[1] > M_PI) || (y[1] < - M_PI)) {
+    if (y[1] > M_PI){
+       y[1] -= 2 * M_PI;
+    }
+    else {
+       y[1] += 2 * M_PI;
+    }
+  }
+
   MatrixXd Ht = H_.transpose();
-  MatrixXd S = H_ * P_ * Ht + R_;
-  MatrixXd Si = S.inverse();
   MatrixXd PHt = P_ * Ht;
+  MatrixXd S = H_ * PHt + R_;
+  MatrixXd Si = S.inverse();
   MatrixXd K = PHt * Si;
     
   //new estimate
